ba6696980e3b549a993e43eb87feaf05b7d49136
Rafael Ferreira <rafaelpcf3@gmail.com>
2020-10-28 13:30:16 -0300

    Arrumado Url

--- src/main/java/br/com/aps_rest_api/service/helpers/UrlConstant.java
+++ src/main/java/br/com/aps_rest_api/service/helpers/UrlConstant.java
@@ -3,6 +3,6 @@ package br.com.aps_rest_api.service.helpers;
 public class UrlConstant {
 
     //public static String URL = "http://localhost:8082";
-    public static String URL = "http://15.228.34.148/8082";
+    public static String URL = "http://15.228.34.148:8082";
 
 }
